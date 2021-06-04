6e5fe400185f7c554774549bb9ae3154c5f27383
zikwall <andrey.kapitonov.96@gmail.com>
2021-06-04 15:20:40 +0300

    Fix typo

--- client.go
+++ client.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"github.com/zikwall/fsclient/errors"
 	"github.com/zikwall/fsclient/requesters"
-	"log"
 	"net/url"
 	"os"
 )
@@ -54,31 +53,4 @@ func (fs FsClient) SendFile(context context.Context, files ...*os.File) error {
 	}
 
 	return fs.client.SendFile(context, files...)
-}
-
-func main() {
-	fsclient, err := WithCopyFsClient(FsClient{
-		Uri:        "http://localhost:1337/",
-		SecureType: TypeToken,
-		TokenType:  requesters.TokenTypeQuery,
-		Token:      "changemeplease123",
-		User:       "qwx1337",
-		Password:   "123456",
-	})
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	f1, _ := os.Open("test.txt")
-	f2, _ := os.Open("test2.txt")
-
-	defer func() {
-		f1.Close()
-		f2.Close()
-	}()
-
-	if err := fsclient.SendFile(context.Background(), f1, f2); err != nil {
-		log.Fatal(err)
-	}
 }
